12ff36d1da3e91097ab5534e9a684795946aa173
viniciusrufop <rocha.g.vinicius@gmail.com>
2019-07-13 14:52:29 -0300

    revert cors

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -14,7 +14,7 @@ class CorsMiddleware
      */
     public function handle($request, Closure $next)
     {
-        /* $headers = [
+        $headers = [
             'Access-Control-Allow-Origin'      => '*',
             'Access-Control-Allow-Methods'     => 'POST, GET, OPTIONS, PUT, DELETE',
             'Access-Control-Allow-Credentials' => 'true',
@@ -33,24 +33,6 @@ class CorsMiddleware
             $response->header($key, $value);
         }
 
-        return $response; */
-        $allowedDomains = array("https://fechadura.rochaut.com.br");
-        $origin = $request->server('HTTP_ORIGIN');
-        if(in_array($origin, $allowedDomains)){
-            //Intercepts OPTIONS requests
-            if($request->isMethod('OPTIONS')) {
-                $response = response('', 200);
-            } else {
-                // Pass the request to the next middleware
-                $response = $next($request);
-            }
-            // Adds headers to the response
-            $response->header('Access-Control-Allow-Origin', $origin);
-            $response->header('Access-Control-Allow-Methods', 'OPTIONS, HEAD, GET, POST, PUT, PATCH, DELETE');
-            $response->header('Access-Control-Allow-Headers', $request->header('Access-Control-Request-Headers'));
-        }
-
-        // Sends it
         return $response;
     }
 }
